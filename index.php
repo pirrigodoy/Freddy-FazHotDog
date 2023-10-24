cc35e9cbd4ec38c2f69b4f48db71a8c7d2695f49
Pierre Godoy <pirrigodoy02@gmail.com>
2023-10-24 11:28:19 +0200

    24/10/2023 - Pierre

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    require_once './functions/functions-structure.php';
+    require_once './functions/functions.php';
+
+    myHeader();
+    myMenu();
+
+?>
+
+<body>
+    <center><h1>Freddy Faz HotDog</h1></center>
+    <br>
+    <div class="container text-center">
+        <div class="row">
+            <?php
+            ?>
+        </div>
+    </div>
+</body>
