e43827918ca8e0424df2c1f77dad8de86345feb9
Freddy Achata Rojas <frac2501@proven.local>
2023-10-25 12:40:29 +0200

    Modifico carta.php

--- functions/carta.php
+++ functions/carta.php
@@ -10,15 +10,18 @@ $carta = [
     "Cocacola"=>["precio"=>1.35,"tipo"=>"bebida","source"=>"Cocacola.png"],
     "Nestea"=>["precio"=>1.25,"tipo"=>"bebida","source"=>"Nestea.png"],
 ];
-
+$hotDog = [
+    "Hot Dog Picante"=>["salchicha"=>$ingredientes[2],"pan"=>$ingredientes[5],"topping" => $ingredientes[6], "source"=>"HotDogPicante.png"],
+];
 $ingredientes=[
-    "Salchicha Deluxe" =>["precio"=>2,"tipo"=>"salchicha", "source"=>"Salchicha.png"],
-    "Salchicha Classic" =>["precio"=>1.5,"tipo"=>"salchicha", "source"=>"Salchicha.png"],
-    "Salchicha Basic" =>["precio"=>1,"tipo"=>"salchicha", "source"=>"Salchicha.png"],
-    "Pan Brioche" =>["precio"=>1,"tipo"=>"pan", "source"=>"Salchicha.png"],
-    "Pan Classic" =>["precio"=>0.5,"tipo"=>"pan", "source"=>"Salchicha.png"],
-    "Topping 1" =>["precio"=>0.5,"tipo"=>"pan", "source"=>"Salchicha.png"],
-    "Topping 2" =>["precio"=>0.5,"tipo"=>"pan", "source"=>"Salchicha.png"],
-    "Topping 3" =>["precio"=>0.5,"tipo"=>"pan", "source"=>"Salchicha.png"],
+    "Salchicha Deluxe" =>["precio"=>2,"tipo"=>"salchicha", "source"=>"SalchichaDeluxe.png"],
+    "Salchicha Vegetariana" =>["precio"=>1.5,"tipo"=>"salchicha", "source"=>"SalchichaVegetariana.png"],
+    "Salchicha Picante" =>["precio"=>1.5,"tipo"=>"salchicha", "source"=>"SalchichaPicante.png"],
+    "Salchicha Classic" =>["precio"=>1,"tipo"=>"salchicha", "source"=>"SalchichaClassic.png"],
+    "Pan Brioche" =>["precio"=>1,"tipo"=>"pan", "source"=>"PanBrioche.png"],
+    "Pan Classic" =>["precio"=>0.5,"tipo"=>"pan", "source"=>"PanClassic.png"],
+    "Topping 1" =>["precio"=>0.5,"tipo"=>"pan", "source"=>"Topping1.png"],
+    "Topping 2" =>["precio"=>0.5,"tipo"=>"pan", "source"=>"Topping2.png"],
+    "Topping 3" =>["precio"=>0.5,"tipo"=>"pan", "source"=>"Topping3.png"],
 ]
 ?>
